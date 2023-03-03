d7ef5dad2c73cc9fc7004ab5165c8843725b538a
Maksim Valentinovich <maks0076@mail.ru>
2023-03-03 20:47:00 +0300

    push Tests

---
+++ TestCheckText/CheckTextTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using New_Structure;
+using System;
+
+namespace TestCheckText
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidateText_text_bool_returned()
+        {
+            ///arrege
+            string text = " ";
+
+            bool extendet = false;
+
+            ///act 
+
+            ValidateText validate = new ValidateText();
+
+            bool actual = validate.CheckSymbols(text);
+
+            ///assert
+            Assert.AreEqual(extendet, actual);
+        }
+    }
+}
---
+++ TestCountWords/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCountWords")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCountWords")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5b2270f-ec68-45cf-9e67-a38fd85300bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
